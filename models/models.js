74e8425b95c872b6b4e0ded375849f79df50819f
danPereiro <danpereiro@gmail.com>
2015-08-11 21:20:46 +0200

    preparado para Heroku postgres

--- models/models.js
+++ models/models.js
@@ -1,15 +1,33 @@
 var path = require('path');
 
+//adaptamos modelo a despliegue en heroku
+var url = process.env.DATABASE_URL.match(/(.*))\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name   = (url[6]||null);
+var user      = (url[2]||null);
+var pwd       = (url[3]||null);
+var protocol  = (url[1]||null);
+var dialect   = (url[1]||null);
+var port      = (url[5]||null);
+var host      = (url[4]||null);
+var storage   = process.env.DATABASE_STORAGE;
+
 //esto carga el modelo ORM
 var Sequelize = require('sequelize');
 
-//marcamos SQLite como bbdd
-var sequelize = new Sequelize(null, null, null,
-                      {dialect: 'sqlite', storage: 'quiz.sqlite'}
-                );
+//Elegimos que BBDD usamos
+var sequelize = new Sequelize(DB_name,user,pwd,
+  { dialect:   protocol,
+    protocol:  protocol,
+    port:      port,
+    host:      host,
+    storage:   storage, //solo SQLite (.env)
+    omitNull:  true //solo Postgres
+  }
+);
 
 //Importamos la definicion de la tabla QUIZ en quiz.js
-var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
 
 //exportamos la definicion
 exports.Quiz = Quiz;
