8ed27732a39a8cddc2e8d6ae46d27ccebdb011eb
danPereiro <danpereiro@gmail.com>
2015-08-14 01:54:24 +0200

    fin modulo7

--- models/models.js
+++ models/models.js
@@ -21,7 +21,8 @@ var sequelize = new Sequelize(DB_name,user,pwd,
     port:      port,
     host:      host,
     storage:   storage, //solo SQLite (.env)
-    omitNull:  true //solo Postgres
+    omitNull:  true, //solo Postgres
+    ssl:       true
   }
 );
 
