bdc66803255f8ddfa84b7b693227c7371e3d645d
danPereiro <danpereiro@gmail.com>
2015-08-14 03:02:49 +0200

    fin modulo7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,46 @@
 var models = require('../models/models.js');
 
+//Autoload - para factorizar codigo si ruta incluye :quizId
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.find(quizId).then(
+    function(quiz) {
+      if (quiz) {
+        req.quiz = quiz;
+        next();
+      } else {
+        next(new Error('No existe quizId=' + quizId));
+      }
+    }
+  ).catch(function(error) { next(error);});
+};
+
+exports.index = function(req, res) {
+  models.Quiz.findAll().then(function(quizes) {
+    res.render('quizes/index.ejs', { quizes: quizes});
+  });
+};
+
+//GET /quizes/:id
+exports.show = function(req, res) {
+  models.Quiz.find(req.params.quizId).then(function(quiz) {
+    res.render('quizes/show', {quiz: quiz});
+  });
+};
+
+//Get para /quizes/:id/answer
+exports.answer = function(req, res) {
+  models.Quiz.find(req.params.quizId).then(function(quiz) {
+    if(req.query.respuesta === quiz.respuesta) {
+      res.render('quizes/answer',
+                  {quiz:quiz, respuesta: 'Correcto'});
+    } else {
+      res.render('quizes/answer',
+                  {quiz:quiz, respuesta: 'Incorrecto'});
+    }
+  });
+};
+
+/* Viejo codigo para funcionamiento con una sola pregunta
 //Get para /quizes/question
 exports.question = function(req, res) {
   models.Quiz.findAll().success(function(quiz) {
@@ -17,3 +58,4 @@ exports.answer = function(req, res) {
     }
   });
 };
+*/
