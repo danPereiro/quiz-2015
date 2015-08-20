76513824227a81460016c0abb3facb4b16c8adfb
danPereiro <danpereiro@gmail.com>
2015-08-20 04:08:08 +0200

    Ejercicio 8 completo

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -53,7 +53,7 @@ exports.answer = function(req, res) {
 
 exports.new = function(req, res) {
   var quiz = models.Quiz.build( //crea objeto quiz
-    {pregunta:"Pregunta", respuesta:"Respuesta"}
+    {pregunta:"Pregunta", respuesta:"Respuesta", tema:"Tema"}
   );
   res.render('quizes/new',{quiz:quiz, errors:[]});
 };
@@ -69,7 +69,7 @@ exports.create = function(req,res) {
         res.render('quizes/new',{quiz: quiz, errors: err.errors});
       } else {
         quiz
-        .save({fields:["pregunta", "respuesta"]})
+        .save({fields:["pregunta", "respuesta", "tema"]})
         .then(function(){res.redirect('/quizes')})
       }
     }
@@ -86,6 +86,7 @@ exports.edit = function(req, res) {
 exports.update = function(req, res) {
   req.quiz.pregunta = req.body.quiz.pregunta;
   req.quiz.respuesta = req.body.quiz.respuesta;
+  req.quiz.tema = req.body.quiz.tema;
   req.quiz
   .validate()
   .then(
@@ -94,12 +95,20 @@ exports.update = function(req, res) {
         res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
       } else {
         req.quiz
-        .save( {fields: ["pregunta", "respuesta"]})
+        .save( {fields: ["pregunta", "respuesta", "tema"]})
         .then( function(){ res.redirect('/quizes');});
       }
     }
   );
 };
+
+//DELETE /quizes/:id
+exports.destroy = function(req, res) {
+  req.quiz.destroy().then( function() {
+    res.redirect('/quizes');
+  }).catch(function(error){next(error)});
+};
+
 /* Viejo codigo para funcionamiento con una sola pregunta
 //Get para /quizes/question
 exports.question = function(req, res) {
